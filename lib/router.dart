41a3a7da6c3935b49b5b1f4974e61ac364a0ea17
asm2212 <asmareadmasu0@gmail.com>
2024-09-21 11:08:47 +0300

    setting up themes,folder structure and routes

---
+++ lib/router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shoapp/features/auth/screens/auth_screen.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings){
+  switch (routeSettings.name) {
+  case AuthScreen.routeName:
+       return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AuthScreen(),
+        );
+  default:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const Scaffold(
+          body:  Center(
+            child: Text('Page not found'),
+          ),
+        ),
+        );
+
+}
+}
