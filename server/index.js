db764ef59453de9547ddbcea447515b33ec88e9a
asm2212 <asmareadmasu0@gmail.com>
2024-09-22 08:22:20 +0300

    middleware,signup routing,connecting to mongodb and creating user model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const authRouter = require("./routes/auth");
+const mongoose = require("mongoose");
+
+
 
 const app = express();
-
 PORT = 3000;
+MONGO_DB = "mongodb://0.0.0.0:27017/shoapp";
+
+app.use(express.json());
+app.use(authRouter);
+
+mongoose.connect(MONGO_DB, {
+    
+})
+   .then(() => console.log("Connected to MongoDB"))
+   .catch(err => console.log(err));
+
 
 app.listen(PORT,"0.0.0.0" ,() => {
     console.log(`Server is running on port ${PORT}`);
---
+++ server/routes/auth.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const User = require("../models/user");
+
+const authRouter = express.Router();
+
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+module.exports = authRouter;
