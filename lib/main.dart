a10814c8af125b4ad20de3658f558c6368eb6d03
asm2212 <asmareadmasu0@gmail.com>
2024-09-24 03:59:20 +0300

    Implement JWT state persistence using Provider and SharedPreferences for client-side authentication

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shoapp/constants/global_variables.dart';
 import 'package:shoapp/features/auth/screens/auth_screen.dart';
+import 'package:shoapp/features/auth/services/auth_service.dart';
+import 'package:shoapp/features/home/screens/home_screen.dart';
 import 'package:shoapp/providers/user_provider.dart';
 import 'package:shoapp/router.dart';
 
@@ -14,8 +16,21 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+  
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +52,9 @@ class MyApp extends StatelessWidget {
           ),
         ),
         onGenerateRoute: (settings) => generateRoute(settings),
-        home:  const AuthScreen(),
+        home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? const HomeScreen()
+          : const AuthScreen(),
       ),
     );
   }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const auth = require("../middlewares/auth");
 
 const authRouter = express.Router();
 
@@ -47,6 +48,27 @@ authRouter.post("/api/signin", async (req, res) => {
     res.status(500).json({ error: e.message });
   }
   });
+
+  authRouter.post("/tokenIsValid", async (req, res) => {
+    try {
+      const token = req.header("x-auth-token");
+      if (!token) return res.json(false);
+      const verified = jwt.verify(token, "passwordKey");
+      if (!verified) return res.json(false);
+  
+      const user = await User.findById(verified.id);
+      if (!user) return res.json(false);
+      res.json(true);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+
+authRouter.get("/", auth, async (req, res) => {
+    const user = await User.findById(req.user);
+    res.json({ ...user._doc, token: req.token });
+  });
+
 
 
 module.exports = authRouter;
